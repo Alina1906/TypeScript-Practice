ad7e2b4655eac341b4d18167462dbb7ae89f66a0
Alina1906 <airgear1906@gmail.com>
2024-08-01 00:13:49 +0300

    test modified

--- tests/singleton.test.ts
+++ tests/singleton.test.ts
@@ -1,6 +1,6 @@
 import {singleton} from "../src/patterns"
 
-describe('Singleton Test', () => {
+describe('KAN_T1_Singleton Test', () => {
     it('should return true working with the instance', () => {
         const result = singleton()
         expect(result).toBe(true)
